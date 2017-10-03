62d2a6f075ae9a489fa0db6105823dca7030ef76
M.Suska <msuska@gmail.com>
2017-10-04 00:30:53 +0200

    added main dashboard component

--- Picnic.Net.Dashboard/Picnic.Net.Dashboard.API/wwwroot/main.bundle.js
+++ Picnic.Net.Dashboard/Picnic.Net.Dashboard.API/wwwroot/main.bundle.js
@@ -17,6 +17,35 @@ webpackEmptyAsyncContext.id = "../../../../../src/$$_gendir lazy recursive";
 
 /***/ }),
 
+/***/ "../../../../../src/app/app-dashboard/app-dashboard.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppDashboard; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var AppDashboard = (function () {
+    function AppDashboard() {
+    }
+    return AppDashboard;
+}());
+AppDashboard = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+        selector: 'app-dashboard',
+        template: "<hello-world></hello-world>\n    <date></date>"
+    })
+], AppDashboard);
+
+//# sourceMappingURL=app-dashboard.component.js.map
+
+/***/ }),
+
 /***/ "../../../../../src/app/app.module.ts":
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
@@ -26,14 +55,16 @@ webpackEmptyAsyncContext.id = "../../../../../src/$$_gendir lazy recursive";
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__("../../../forms/@angular/forms.es5.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_http__ = __webpack_require__("../../../http/@angular/http.es5.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__hello_world_hello_world_component__ = __webpack_require__("../../../../../src/app/hello-world/hello-world.component.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__date_date_component__ = __webpack_require__("../../../../../src/app/date/date.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__app_dashboard_app_dashboard_component__ = __webpack_require__("../../../../../src/app/app-dashboard/app-dashboard.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__hello_world_hello_world_component__ = __webpack_require__("../../../../../src/app/hello-world/hello-world.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__date_date_component__ = __webpack_require__("../../../../../src/app/date/date.component.ts");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+
 
 
 
@@ -48,8 +79,9 @@ var AppModule = (function () {
 AppModule = __decorate([
     Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["L" /* NgModule */])({
         declarations: [
-            __WEBPACK_IMPORTED_MODULE_4__hello_world_hello_world_component__["a" /* HelloWorldComponent */],
-            __WEBPACK_IMPORTED_MODULE_5__date_date_component__["a" /* DateComponent */]
+            __WEBPACK_IMPORTED_MODULE_4__app_dashboard_app_dashboard_component__["a" /* AppDashboard */],
+            __WEBPACK_IMPORTED_MODULE_5__hello_world_hello_world_component__["a" /* HelloWorldComponent */],
+            __WEBPACK_IMPORTED_MODULE_6__date_date_component__["a" /* DateComponent */]
         ],
         imports: [
             __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
@@ -57,7 +89,7 @@ AppModule = __decorate([
             __WEBPACK_IMPORTED_MODULE_3__angular_http__["b" /* HttpModule */]
         ],
         providers: [],
-        bootstrap: [__WEBPACK_IMPORTED_MODULE_4__hello_world_hello_world_component__["a" /* HelloWorldComponent */], __WEBPACK_IMPORTED_MODULE_5__date_date_component__["a" /* DateComponent */]]
+        bootstrap: [__WEBPACK_IMPORTED_MODULE_4__app_dashboard_app_dashboard_component__["a" /* AppDashboard */]]
     })
 ], AppModule);
 
